831635aa1dae16e91ea8593025da7db383f7afac
AcheZeta <ache.mariana@gmail.com>
2019-03-30 10:18:57 -0600

    index.js comentado

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,29 @@
-login: () => {
-    (function () {
-      var ui = new firebaseui.auth.AuthUI(firebase.auth());
-      var uiConfig = {
-        callbacks: {
-          signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+// login: () => {
+//     (function () {
+//       var ui = new firebaseui.auth.AuthUI(firebase.auth());
+//       var uiConfig = {
+//         callbacks: {
+//           signInSuccessWithAuthResult: function (authResult, redirectUrl) {
 
-            return true;
-          },
-          uiShown: function () {
-            document.getElementById('loader').style.display = 'none';
-          }
-        },
-        signInFlow: 'popup',
-        signInSuccessUrl: 'index.html#/forum',
-        signInOptions: [
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        ],
+//             return true;
+//           },
+//           uiShown: function () {
+//             document.getElementById('loader').style.display = 'none';
+//           }
+//         },
+//         signInFlow: 'popup',
+//         signInSuccessUrl: 'index.html#/forum',
+//         signInOptions: [
+//           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//           firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//         ],
 
 
-        // Terms of service url.
-        tosUrl: 'index.html#/forum',
-        // Privacy policy url.
-        //privacyPolicyUrl: '<your-privacy-policy-url>'
+//         // Terms of service url.
+//         tosUrl: 'index.html#/forum',
+//         // Privacy policy url.
+//         //privacyPolicyUrl: '<your-privacy-policy-url>'
 
-      };
-      ui.start('#firebaseui-auth-container', uiConfig);
-    })()
+//       };
+//       ui.start('#firebaseui-auth-container', uiConfig);
+//     })()
