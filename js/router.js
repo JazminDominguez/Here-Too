6aae0512e69706cd3cb1d9776e15652c3958db35
AcheZeta <ache.mariana@gmail.com>
2019-03-30 09:59:41 -0600

    Router comentado

--- js/router.js
+++ js/router.js
@@ -1,12 +1,12 @@
-window.libreria.getId('vista').enrutar()
-    .ruta('/', './Vistas/primera.html', 'miControlador', function(){
-        libreria.getctrl().primerap()
-    })
+// window.libreria.getId('vista').enrutar()
+//     .ruta('/', './Vistas/primera.html', 'miControlador', function(){
+//         libreria.getctrl().primerap()
+//     })
 
-    .ruta('/segunda', './Vistas/segunda.html', 'miControlador', function(){
-        libreria.getctrl().segundap()
-    })
+//     .ruta('/segunda', './Vistas/segunda.html', 'miControlador', function(){
+//         libreria.getctrl().segundap()
+//     })
 
-    .ruta('/tercera', './Vistas/tercera.html', 'miControlador', function(){
-        libreria.getctrl().prueba()
-    })
+//     .ruta('/tercera', './Vistas/tercera.html', 'miControlador', function(){
+//         libreria.getctrl().prueba()
+//     })
